553f0be84bf5a357fdad4a8eb294c6c7f2960546
Ryan James <realcoder2023@gmail.com>
2025-05-16 18:52:55 -0400

    Add project files.

---
+++ PlacesVisited/PlacesVisited.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+enum Hobbies {
+	Coding,
+	Family,
+	Working,
+	Dog
+};
+
+enum Visited {
+	NYC,
+	Vermont,
+	NiagaraFalls,
+	Florida
+};
+
+
+
+int main() {
+
+	Hobbies hob{};
+	std::vector<Hobbies> hobbies{ Hobbies::Coding, Hobbies::Dog, Hobbies::Working, Hobbies::Family };
+
+	Visited vis{};
+	std::vector<Visited> visit{ Visited::Florida,Visited::NYC, Visited::NiagaraFalls };
+
+	for (auto& visited : visit) {
+
+		switch (visited) {
+
+		case Visited::Florida:
+			cout << "1: Florida" << endl;
+			break;
+
+		case Visited::NiagaraFalls:
+			cout << "2: NiagaraFalls" << endl;
+		
+		case Visited::Vermont:
+			cout << "3: Vermont" << endl;
+		
+		case Visited::NYC:
+			cout << "4: NYC" << endl;
+		
+		}
+
+	}
+
+	cout << endl;
+
+	for (auto& ho : hobbies) {
+
+		switch (ho) {
+
+		case Hobbies::Coding :
+			cout << "1: Coding" << endl;
+			break;
+
+		case Hobbies::Dog:
+				
+			cout << "2: Dog" << endl;
+			break;
+
+		case Hobbies::Family:
+			cout << "3: Family" << endl;
+			break;
+		case Hobbies::Working:
+			cout << "4: Working" << endl;
+			break;
+		}
+	}
+
+	system("pause > 0");
+	return 0;
+}
